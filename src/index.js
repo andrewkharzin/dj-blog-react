d4abda84e46a200af5337ccc2fa812e9814f7e7e
Andrey Kharzin <andrewkharzin@iMac-Andrey.local>
2022-09-01 23:02:25 +0300

    Firts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+       
+      </Routes>
+      <Footer />
+    </React.StrictMode>
+  </Router>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
